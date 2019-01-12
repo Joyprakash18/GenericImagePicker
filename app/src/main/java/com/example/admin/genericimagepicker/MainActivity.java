ea6e7ff8771b1bde004d58b7042cd22979d879e2
Joy Prakash Das <joyprakash.d@encoders.co.in>
2019-01-12 13:32:50 +0530

    Add galary click in MainActivity

--- app/src/main/java/com/example/admin/genericimagepicker/MainActivity.java
+++ app/src/main/java/com/example/admin/genericimagepicker/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         sdCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                handleGallerySelect();
             }
         });
     }
